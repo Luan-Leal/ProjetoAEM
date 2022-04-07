6bd4fcf554fb87fdc403589c442e24079eda7a9b
Luan Leal <luanleal05@gmail.com>
2022-04-07 18:12:41 -0300

    Feat: navbar finished

---
+++ ui.frontend/src/components/Navbar/index.vue
@@ -0,0 +1,24 @@
+<template>
+   <div id="app">
+    <nav>
+      <div class="info">
+        <h4>Team Sign Up</h4>
+      </div>
+      <div class="items">
+        <div class="tab active"><li>Basic</li></div>
+        <div class="tab"><li>Social</li></div>
+        <div class="tab"><li>Certificate</li></div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped lang="scss">
+    @import "./index.scss";
+</style>
