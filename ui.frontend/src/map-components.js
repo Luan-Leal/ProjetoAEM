4fde1421a67ffa738991ff254458ec8ffab6bb0f
Luan Leal <luanleal05@gmail.com>
2022-04-07 18:20:21 -0300

    Feat: form finished

--- ui.frontend/src/map-components.js
+++ ui.frontend/src/map-components.js
@@ -1,6 +1,7 @@
 import Text from './components/Text/Text'
 import AppPage from './components/Page/AppPage'
 import Navbar from './components/Navbar/index'
+import GeneralInputs from './components/GeneralInputs/index'
 
 import {
   withComponentMappingContext,
@@ -11,6 +12,8 @@ import {
 MapTo('vue/components/text')(Text)
 
 MapTo('vue/components/navbar')(Navbar)
+
+MapTo('vue/components/general-inputs')(GeneralInputs)
 
 // AppPage Mapping
 MapTo('vue/components/page')(withComponentMappingContext(AppPage))
