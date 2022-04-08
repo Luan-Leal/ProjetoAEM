74bd6db7f83403fe2ab1ed93fa05cb90238c858f
Luan Leal <luanleal05@gmail.com>
2022-04-07 21:15:08 -0300

    Feat: checkbox finished

---
+++ ui.frontend/src/components/CheckBox/index.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <label class="container">{{ text }}
+    <input type="Checkbox" name="checkbox" class="checkbox">
+    <span class="checkmark"></span>
+  </label>
+
+</template>
+
+<script>
+
+export default {
+  name: 'InputCheckbox',
+  props: {
+    text: {
+      type: String,
+      default: 'Texto Padrão'
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+    @import "./index.scss";
+</style>
